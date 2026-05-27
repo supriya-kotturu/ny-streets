51eb3d3c13ff073d7b1a7cb6be7d33c75e2e38d2
supriya-kotturu <supriyakotturu@gmail.com>
2026-05-27 14:10:40 -0700

    feat: update DemoApp to use dynamic item IDs for ArcGIS components

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ import "@arcgis/charts-components/components/arcgis-charts-action-bar";
 
 @customElement("demo-app")
 export class DemoApp extends LitElement {
-  private readonly instantChartsUrl =
-    "https://jsapi.maps.arcgis.com/apps/instant/charts/index.html?appid=9765c187fcf2469894377acefcbfcf61";
+  private readonly mapItemId = "170c461c2973474e80d13c1294cc6669";
+  private readonly chartLayerItemId = "65a82f75363d4ab7882bb7301dbeb16a";
 
   static styles = css`
     :host {
@@ -35,6 +35,16 @@ export class DemoApp extends LitElement {
     }
   `;
 
+  // By default, LitElement uses Shadow DOM, which keeps a component’s styles separate from the rest of the page.
+  // But in this case, we don’t want that. We override createRenderRoot so the component renders in the regular DOM (light DOM) instead.
+
+  // This lets the page’s main styles apply to the component, which is required for ArcGIS components to display correctly.
+  // If we used Shadow DOM, we’d have to manually include those styles inside it, which is more complicated.
+  // So, rendering in the light DOM is the easier fix.
+
+  // NOTE: Without this method override, the <arcgis-chart> component won’t render properly because it won’t get the right styles.
+  // The map will still render, but the chart will be broken.
+
   protected override createRenderRoot() {
     return this;
   }
@@ -44,19 +54,16 @@ export class DemoApp extends LitElement {
       <calcite-shell>
         <calcite-shell-panel slot="panel-start">
           <calcite-panel heading="Map details">
-            <div class="panel-content">add filters, metadata, or controls, etc.</div>
+            <div class="panel-content">placeholder for filters, metadata, or controls, etc.</div>
           </calcite-panel>
         </calcite-shell-panel>
-        <arcgis-map item-id="170c461c2973474e80d13c1294cc6669">
-        </arcgis-map>
+        <arcgis-map item-id="${this.mapItemId}"> </arcgis-map>
         <calcite-shell-panel slot="panel-end">
-          <calcite-panel heading="Related chart"> <arcgis-map item-id="170c461c2973474e80d13c1294cc6669">
-          <arcgis-chart
-            layer-item-id="65a82f75363d4ab7882bb7301dbeb16a"
-            chart-index="0"
-            slot="popup">
-          <arcgis-charts-action-bar slot="action-bar"></arcgis-charts-action-bar>
-          </arcgis-chart></calcite-panel>
+          <calcite-panel heading="Related chart">
+            <arcgis-chart layer-item-id="${this.chartLayerItemId}" chart-index="0">
+              <arcgis-charts-action-bar slot="action-bar"> </arcgis-charts-action-bar>
+            </arcgis-chart>
+          </calcite-panel>
         </calcite-shell-panel>
       </calcite-shell>
     `;
